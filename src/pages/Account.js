a13f53e3ec9c56097aa0f08a9730c0577a594917
Chelo <marcelodeleongo@gmail.com>
2022-11-13 01:01:28 -0600

    Account css

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -17,9 +17,53 @@ export default function Account(){
     }
 
     return(
-        <div className='container_center'>
-            <div className='container_center_blue'>
-                
+        <div className='background'>
+            <div className='selections'>
+                <div className='selections_item'>
+                    <div className='selections_item_left'>
+                        <a href='/login' id='package'>
+                            <img src={require('../img/package.png')} id='package'/>
+                        </a>
+                    </div>
+                    <div className='selections_item_right'>
+                        <a href='/login' className='selections_item_title'>
+                            <span>Mis servicios</span>
+                        </a>
+                        <a href='/login' className='selections_item_description'>
+                            <span>Revisa el estado del servicio que hayas contratado</span>
+                        </a>
+                    </div>
+                </div>
+                <div className='selections_item'>
+                    <div className='selections_item_left'>
+                        <a href='/login' id='package'>
+                            <img src={require('../img/serviciocliente.png')} id='package'/>
+                        </a>
+                    </div>
+                    <div className='selections_item_right'>
+                        <a href='/login' className='selections_item_title'>
+                            <span>Servicio al cliente</span>
+                        </a>
+                        <a href='/login' className='selections_item_description'>
+                            <span>Contacta con un socio para resolver dudas que tengas</span>
+                        </a>
+                    </div>
+                </div>
+                <div id='data' className='selections_item'>
+                    <div className='selections_item_left'>
+                        <a href='/login' id='package'>
+                            <img src={require('../img/cuenta.png')} id='package'/>
+                        </a>
+                    </div>
+                    <div className='selections_item_right'>
+                        <a href='/login' className='selections_item_title'>
+                            <span>Mi cuenta</span>
+                        </a>
+                        <a href='/login' className='selections_item_description'>
+                            <span>Tus datos</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
