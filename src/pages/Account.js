3cbc7b92a22b1c5e237e95ba4f998af1b39f7a46
Chelo <marcelodeleongo@gmail.com>
2022-12-13 20:18:19 -0600

    Implementacion de localizacion backend

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -12,15 +12,24 @@ export default function Account(){
     const [newLocationId, setNewLocationId] = useState(null);
     const [locationName, setLocationName] = useState(null);
     const [isChangingLocation, setIsChangingLocation] = useState(false);
+    const [isAnotherLocation, setIsAnotherLocation] = useState(false);
     const [countries, setCountries] = useState(null);
     const [newCountry, setNewCountry] = useState(null);
     const [states, setStates] = useState(null);
+    const [locations, setLocations] = useState(null);
     const [newState, setNewState] = useState(null);
     const [newLocation, setNewLocation] = useState(null);
+    const [newLocationAnother, setNewLocationAnother] = useState(null);
     const [renderStates, setRenderStates] = useState(false);
     const [renderInput, setRenderInput] = useState(false);
     const [renderSubmit, setRenderSubmit] = useState(false);
+    const [gotLocation, setGotLocation] = useState(false);
+    var newLocationIdInsert;
     var locationId = null;
+    var newCountryName;
+    var newCountryId;
+    var newStateId;
+    var locationsArray = [];
     
     const [loadingScreen, setLoadingScreen] = useState(true);
 
@@ -46,15 +55,13 @@ export default function Account(){
     }
 
     const updateLocation = async () => {
-        if(newLocationId == 0 || newLocationId == null){
-            window.alert("Por favor selecciona una localización válida");
-            return;
-        }
-        else{
-            const { data: { user } } = await supabase.auth.getUser();
+        const { data: { user } } = await supabase.auth.getUser();
+        console.log(user);
+        console.log(newLocationIdInsert);
+        if(newLocationIdInsert){
             const { data, error } = await supabase
             .from('account')
-            .update({ location_id: newLocationId })
+            .update({ location_id: newLocationIdInsert })
             .eq('uuid', user.id);
             document.location.reload();
         }
@@ -74,14 +81,134 @@ export default function Account(){
     }
 
     const getStatesData = async () => {
-        console.log(newCountry);
-        console.log("Entra getStatesData");
         let data = require('../assets/country-states-data.json');
         setStates(data[newCountry].states);
+    }
+    
+    const getLocationData = async () => {
+        if(!gotLocation){
+            setGotLocation(true);
+            const { data, error } = await supabase
+            .from('states')
+            .select('id')
+            .eq('name', newState);
+            if(data.length == 0){
+                console.log("nodata");
+                setLocations([{name: 'Localización'}, {name: 'Otro'}])
+            }
+            newStateId = data[0].id;
+            getLocations();
+        }
+    }
+
+    const getLocations = async () => {
+        const { data, error } = await supabase
+        .from('location')
+        .select()
+        .eq('state_id', newStateId);
+        locationsArray = data;
+        locationsArray.push({name: 'Otro'});
+        let newArray = [
+            ...locationsArray.slice(0, 0),
+            {name: 'Localización'},
+            ...locationsArray.slice(0)
+        ]
+        setLocations(newArray);
+        if(data.length == 0){
+            console.log("VACIO");
+        }
+        else{
+            console.log("HAY");
+        }
     }
 
     const changeLocation = async () => {
         window.alert('Cambio de localización');
+        checkCountry();
+        updateLocation();
+    }
+
+    const checkCountry = async () => {
+        newCountryName = countries[newCountry].country_name;
+        const { data, error } = await supabase
+        .from('countries')
+        .select()
+        .eq('name', newCountryName);
+        if(data.length == 0){
+            console.log("No existe pais");
+            const { error } = await supabase
+            .from('countries')
+            .insert({ name: newCountryName });
+            console.log(error);
+            checkCountry();
+        }
+        else{
+            newCountryId = data[0].id;
+            checkState();
+        }
+    }
+
+    const checkState = async () => {
+        const { data, error } = await supabase
+        .from('states')
+        .select()
+        .eq('name', newState);
+        if(data.length == 0){
+            console.log("No existe estado");
+            const { error } = await supabase
+            .from('states')
+            .insert({ name: newState, country_id: newCountryId});
+            console.log(error);
+            checkState();
+        }
+        else{
+            checkLocation();
+        }
+    }
+
+    const checkLocation = async () => {
+        for(var i = 0 ; i < locations.length ; i++){
+            if(isAnotherLocation){
+                if(locations[i].name == newLocationAnother){
+                    console.log("Ya existe uno igual");
+                    newLocationIdInsert = locations[i].id;
+                    updateLocation();
+                    return;
+                }
+            }
+            else{
+                if(locations[i].name == newLocation){
+                    console.log("Ya existe uno igual no anotherloc");
+                    console.log(locations[i].id);
+                    newLocationIdInsert = locations[i].id;
+                    updateLocation();
+                    return;
+                }
+            }
+        }
+        console.log("PASA POR AQUI");
+        const { data, error } = await supabase
+        .from('states')
+        .select('id')
+        .eq('name', newState);
+        newStateId = data[0].id;
+        insertLocation();
+        checkNewLocation();
+    }
+
+    const checkNewLocation = async () => {
+        const { data, error } = await supabase
+        .from('location')
+        .select('id')
+        .eq('name', newLocationAnother);
+        newLocationIdInsert = data[0].id;
+        updateLocation();
+    }
+
+    const insertLocation = async () => {
+        const { error } = await supabase
+        .from('location')
+        .insert({ name: newLocationAnother, state_id: newStateId});
     }
     
     useEffect(() => {
@@ -93,12 +220,29 @@ export default function Account(){
             setRenderStates(false);
         }
         if(newState){
+            getLocationData();
             setRenderInput(true);
         }
         else{
             setRenderInput(false);
         }
+        if(newLocationAnother){
+            setRenderSubmit(true);
+        }
+        else{
+            setRenderSubmit(false);
+        }
         if(newLocation){
+            if(newLocation == "Localización"){
+                setNewLocation(null);
+                setIsAnotherLocation(false);
+                return;
+            }
+            if(newLocation == "Otro"){
+                setIsAnotherLocation(true);
+                return;
+            }
+            setIsAnotherLocation(false);
             setRenderSubmit(true);
         }
         else{
@@ -160,10 +304,11 @@ export default function Account(){
                             {renderStates &&
                                 <div className='account_is_changing_location_item'>
                                     <span>Estado</span>
-                                    <select onChange={(e) => {setNewState(e.target.value); setNewLocation(null)}} className="account_is_changing_location_select">
+                                    <select onChange={(e) => {setNewState(e.target.value); setNewLocation(null); getLocationData(); setLocations([]); 
+                                        setGotLocation(false)}} className="account_is_changing_location_select">
                                     {states.map((state) => {
                                         return(
-                                            <option value={state.state_id}>{state.state_name}</option>
+                                            <option value={state.state_name}>{state.state_name}</option>
                                         )
                                     })}
                                     </select>
@@ -172,13 +317,26 @@ export default function Account(){
                             {renderInput &&
                                 <div className='account_is_changing_location_item'>
                                     <span>Localidad</span>
+                                    <select onChange={(e) => {setNewLocation(e.target.value)}} className="account_is_changing_location_select">
+                                    {locations.map((location) => {
+                                        return(
+                                            <option value={location.name}>{location.name}</option>
+                                        )
+                                    })}
+                                    </select>
+                                </div>
+                            }
+                            {isAnotherLocation &&
+                                <div className='account_is_changing_location_item'>
+                                    <span>Localidad</span>
                                     <input id="account_is_changing_location_text_input"
                                     type={'text'}
                                     placeholder={"Localización"}
-                                    onChange={(e) => setNewLocation(e.target.value)}
+                                    onChange={(e) => setNewLocationAnother(e.target.value)}
                                     />
                                 </div>
                             }
+                            
                             {renderSubmit &&
                                 <input id="account_is_changing_location_text_submit"
                                 type={'submit'}
