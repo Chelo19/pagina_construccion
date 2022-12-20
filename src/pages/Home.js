6e7f6f78344032c55e34e25c4f4bafdc67d3b43c
Chelo <marcelodeleongo@gmail.com>
2022-12-20 17:07:48 -0600

    Correccion Recover Pass 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,12 +34,7 @@ export default function Home(){
     useEffect(() => {
         supabase.auth.onAuthStateChange(async (event, session) => {
           if (event == "PASSWORD_RECOVERY") {
-            const newPassword = prompt("What would you like your new password to be?");
-            const { data, error } = await supabase.auth
-              .updateUser({ password: newPassword })
-     
-            if (data) alert("Password updated successfully!")
-            if (error) alert("There was an error updating your password.")
+            navigate('/#/update-password/');
           }
         })
     }, [])
