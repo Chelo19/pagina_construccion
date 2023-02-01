d9da42def28eb09e4d681860b6e83de2bff383c2
Chelo <marcelodeleongo@gmail.com>
2023-01-31 19:01:55 -0600

    Modificacion SentCotizaciones

--- src/pages/SentCotizaciones.js
+++ src/pages/SentCotizaciones.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {supabase} from '../supabase/client';
 import {useNavigate} from 'react-router-dom';
-import '../styles/Cotizaciones.css';
+import '../styles/SentCotizaciones.css';
 import { Link } from "react-router-dom";
 import LoadingScreen2 from '../components/LoadingScreen2';
 import _ from "lodash";
@@ -10,17 +10,15 @@ import _ from "lodash";
 export default function SentCotizaciones(){
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
+    const [isRejecting, setIsRejecting] = useState(false);
 
     const [noItems, setNoItems] = useState(false);
-    const [isPopUp, setIsPopUp] = useState(false);
-    const [isPopUpReject, setIsPopUpReject] = useState(false);
     const [prompt, setPrompt] = useState(null);
     const [promptStyle, setPromptStyle] = useState(null);
-    const [dateStyle, setDateStyle] = useState({color: '#0077b6'});
 
     const [cotizaciones, setCotizaciones] = useState(null);
+    const [selectedCotizacion, setSelectedCotizacion] = useState(null);
     const [selectedOption, setSelectedOption] = useState(null);
-    const [selectedCotizacion ,setSelectedCotizacion] = useState(null);
 
 
     useEffect(() => {
@@ -33,15 +31,12 @@ export default function SentCotizaciones(){
     }
 
     const getSentCotizaciones = async (user) => {
-        console.log(user.email);
         const { data, error } = await supabase
         .from('cotizaciones')
         .select(`*, service_id(*)`)
         .or("option_selected.is.null")
         .match({ is_ended: false, account_email: user.email })
         .order('id', { ascending: true });
-        console.log(data);
-        console.log(error);
         setCotizaciones(data);
         if(data.length > 0){
             setIsLoading(false);
@@ -52,56 +47,75 @@ export default function SentCotizaciones(){
         }
     }
 
-    const acceptCotizacion = async () => {
-        const { error } = await supabase
-        .from('cotizaciones')
-        .update({ option_selected: selectedOption })
-        .eq('id', selectedCotizacion);
-        if(!error){
-            setPromptStyle({backgroundColor: '#77DD77'});
-            setPrompt('Cotizacion Aceptada');
-            await timeout(2000);
-            setPrompt(null);
-            document.location.reload();
+    const acceptOption = async () => {
+        if(selectedCotizacion && selectedOption){
+            const { error } = await supabase
+            .from('cotizaciones')
+            .update({ is_sent: true, option_selected: selectedOption })
+            .eq('id', selectedCotizacion.id);
+            if(!error){
+                setPromptStyle({backgroundColor: '#77DD77'});
+                setPrompt(`Has aceptado la opción ${selectedOption}`);
+                await timeout(2000);
+                setPrompt(null);
+                document.location.reload();
+                return;
+            }
+            else{
+                console.log(error);
+                setPromptStyle({backgroundColor: '#161825'});
+                setPrompt('Intenta de nuevo');
+                await timeout(2000);
+                setPrompt(null);
+                return;
+            }
         }
         else{
-            console.log(error);
             setPromptStyle({backgroundColor: '#161825'});
-            setPrompt('Intenta de nuevo');
+            setPrompt('Selecciona una opción');
             await timeout(2000);
             setPrompt(null);
+            return;
         }
     }
-
+    
     const endCotizacion = async () => {
-        const { error } = await supabase
-        .from('cotizaciones')
-        .update({ is_ended: true })
-        .eq('id', selectedCotizacion);
-        if(!error){
-            setPromptStyle({backgroundColor: '#161825'});
-            setPrompt('Cotizacion finalizada');
-            await timeout(2000);
-            setPrompt(null);
-            document.location.reload();
+        console.log("end");
+        if(isRejecting){
+            const { error } = await supabase
+            .from('cotizaciones')
+            .update({ is_ended: true })
+            .eq('id', selectedCotizacion.id);
+            if(!error){
+                setPromptStyle({backgroundColor: '#161825'});
+                setPrompt('Cotización finalizada');
+                await timeout(2000);
+                setPrompt(null);
+                document.location.reload();
+                return;
+            }
+            else{
+                console.log(error);
+                setPromptStyle({backgroundColor: '#161825'});
+                setPrompt('Intenta de nuevo');
+                await timeout(2000);
+                setPrompt(null);
+                return;
+            }
         }
-        else{
-            console.log(error);
-            setPromptStyle({backgroundColor: '#161825'});
-            setPrompt('Intenta de nuevo');
-            await timeout(2000);
-            setPrompt(null);
-        }
-        setPromptStyle({backgroundColor: '#161825'});
-        setPrompt('Cotizacion Finalizada');
-        await timeout(2000);
-        setPrompt(null);
     }
 
-    const selectOption = async (cotizacionId, i) => {
-        setIsPopUp(true);
-        setSelectedCotizacion(cotizacionId);
-        setSelectedOption(i);
+    const setDateStyle = (d) => {
+        var now = new Date();
+        var date = new Date(d);
+        date = date.getTime() + 86098571;   // los 86098571 son para corregir la fecha
+        let diff = now - date;      // 7 dias en milisegundos es = 604800000
+        if(diff > 345600000){       // pasan 4 dias
+            return ({color: '#ff5252'});
+        }
+        else{
+            return ({color: '#0077b6'});
+        }
     }
 
     function timeout(number) {
@@ -111,88 +125,74 @@ export default function SentCotizaciones(){
     return(
         <>
             {!isLoading ?
-                <div className='cotizaciones_background'>
-                    <div className='cotizaciones_container'>
-                        {!noItems ?
+                <div className='sent_cotizaciones_background'>
+                    <div className='sent_cotizaciones_container'>
+                        {!noItems ? 
                         <>
-                            {cotizaciones.map((cotizacion) => {
-                                return(
-                                    <div className='cotizaciones_item' key={cotizacion.id}>
-                                        <div className='sent_cotizaciones_item_container'>
-                                            <span id='cotizaciones_title'>
-                                                {cotizacion.service_id.name}
-                                            </span>
-                                            <a href={`${cotizacion.link_drive}`}>
-                                                <span id='cotizaciones_drive'>
-                                                    Link drive
-                                                </span>
-                                            </a>
-                                            <span id='cotizaciones_instructions'>
-                                                Selecciona la cotización que se adecúe a tus necesidades
-                                            </span>
-                                            <div className='sent_cotizaciones_item_buttons'>
-                                                {_.times(cotizacion.options_length, (i) => (
-                                                    <a className='sent_cotizaciones_item_button' onClick={(e) => selectOption(cotizacion.id, i + 1)} key={i}>
-                                                        {i + 1}
-                                                    </a>
-                                                ))}
-                                            </div>
-                                            <a className='sent_cotizaciones_item_end_cotizacion' onClick={(e) => {setIsPopUpReject(true); setSelectedCotizacion(cotizacion.id)}}>
-                                                <span>Finalizar cotizacion</span>
-                                            </a>
-                                            <span id='sent_cotizaciones_item_end_date' style={dateStyle}>Fecha límite: 20/10/23</span>
+                            {!selectedCotizacion ?
+                            <>
+                                <span className='sent_cotizaciones_title'>Cotizaciones</span>
+                                {cotizaciones.map((cotizacion) => {
+                                    return(
+                                        <div className='sent_cotizaiones_item' key={cotizacion.id} onClick={(e) => setSelectedCotizacion(cotizacion)}>
+                                            <span>{cotizacion.service_id.name}</span>
+                                            <span style={setDateStyle(cotizacion.created_at.split('T')[0])}>{cotizacion.created_at.split('T')[0]}</span>
                                         </div>
+                                    );
+                                })}
+                            </>
+                            : 
+                            <div className='sent_cotizaciones_cotizacion'>
+                                <div className='sent_cotizaciones_cotizacion_content'>
+                                    <span><span className='sent_cotizaciones_cotizacion_orange'>Servicio:</span> {selectedCotizacion.service_id.name}</span>
+                                    <span><span className='sent_cotizaciones_cotizacion_orange'>Descripción del servicio:</span> {selectedCotizacion.service_id.description}</span>
+                                </div>
+                                <span className='sent_cotizaciones_cotizacion_instructions'>A continuación se le presentarán {selectedCotizacion.options_length} opción(es), donde elegirá una dependiendo de lo que haya visto en la carpeta de muestra.</span>
+                                <a href={`${selectedCotizacion.link_drive}`} className='sent_cotizaciones_cotizacion_link'>Link Drive</a>
+                                {(!selectedOption && !isRejecting) &&
+                                    <div className='sent_cotizaciones_cotizacion_buttons'>
+                                        {_.times(selectedCotizacion.options_length, (i) => (
+                                            <a className='sent_cotizaciones_cotizacion_button' onClick={(e) => setSelectedOption(i + 1)} key={i + 1}>
+                                                Cotización: {i + 1}
+                                            </a>
+                                        ))}
+                                        <a className='sent_cotizaciones_cotizacion_button' id='sent_cotizaciones_cotizacion_return' onClick={(e) => {setSelectedCotizacion(null); setSelectedOption(null)}}>
+                                            Regresar
+                                        </a>
+                                        <a className='sent_cotizaciones_cotizacion_button' id='sent_cotizaciones_cotizacion_reject' onClick={(e) => setIsRejecting(true)}>
+                                            Finalizar Cotización
+                                        </a>
                                     </div>
-                                )})}
-                        </> 
+                                }
+                                {(selectedOption && !isRejecting) &&
+                                    <div className='sent_cotizaciones_cotizacion_buttons'>
+                                        <span>¿Estás seguro de que deseas aceptar la opción <span className='sent_cotizaciones_cotizacion_orange'>{selectedOption}</span>?</span>
+                                        <a className='sent_cotizaciones_cotizacion_button' id='sent_cotizaciones_cotizacion_accept' onClick={(e) => acceptOption()}>
+                                            Aceptar
+                                        </a>
+                                        <a className='sent_cotizaciones_cotizacion_button' id='sent_cotizaciones_cotizacion_return' onClick={(e) => setSelectedOption(null)}>
+                                            Regresar
+                                        </a>
+                                    </div>
+                                }
+                                {isRejecting &&
+                                    <div className='sent_cotizaciones_cotizacion_buttons'>
+                                        <span>¿Estás seguro de que deseas finalizar la cotización?</span>
+                                        <a className='sent_cotizaciones_cotizacion_button' id='sent_cotizaciones_cotizacion_reject' onClick={(e) => endCotizacion()}>
+                                            Finalizar
+                                        </a>
+                                        <a className='sent_cotizaciones_cotizacion_button' id='sent_cotizaciones_cotizacion_return' onClick={(e) => setIsRejecting(false)}>
+                                            Regresar
+                                        </a>
+                                    </div>
+                                }
+                            </div>}
+                        </>
                         :
-                        <div className='cotizaciones_no_items'>
-                            <div className='cotizaciones_no_items_container'>
-                                <div className='cotizaciones_no_items_container_text'>
-                                    <span>Aún no tienes cotizaciones</span>
-                                    <span>Explora nuestros servicios <Link to={'/categories/1'} id='cotizaciones_no_items_container_link'>aquí</Link></span>
-                                </div>
-                            </div>
-                        </div>}
+                        <>
+                            No cienes cotizaciones aún
+                        </>}
                     </div>
-                    {isPopUp &&
-                        <div className='popup'>
-                            <div className='popup_container'>
-                                <div className='popup_item_container'>
-                                    <div className='popup_text'>
-                                        <span>¿Estás seguro de que deseas aceptar la cotización {selectedOption}?</span>
-                                    </div>
-                                    <div className='popup_buttons'>
-                                        <div className='popup_button' onClick={acceptCotizacion}>
-                                            <img src={require('../img/aceptar.png')}/>  
-                                        </div>
-                                        <div className='popup_button' onClick={(e) => {setIsPopUp(false); setSelectedCotizacion(null); setSelectedOption(null)}}>
-                                            <img src={require('../img/rechazar.png')}/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    }
-                    {isPopUpReject &&
-                        <div className='popup'>
-                            <div className='popup_container'>
-                                <div className='popup_item_container'>
-                                    <div className='popup_text'>
-                                        <span>¿Estás seguro de que quieres finalizar la cotización?</span>
-                                    </div>
-                                    <div className='popup_buttons'>
-                                        <div className='popup_button' onClick={endCotizacion}>
-                                            <img src={require('../img/aceptar.png')}/>  
-                                        </div>
-                                        <div className='popup_button' onClick={(e) => setIsPopUpReject(false)}>
-                                            <img src={require('../img/rechazar.png')}/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    }
                     {prompt &&
                         <div className="reg_log_prompt" style={promptStyle}>
                             {prompt}
